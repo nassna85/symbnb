6cd31b35eb09c09b2076be8acf347cff6aab34b8
Naïm <nassna85@hotmail.fr>
2019-02-13 14:31:31 +0100

    Ajout de la logique des rôles et de la sécurité

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -30,6 +32,7 @@ class AdController extends AbstractController
      * Permet de creer une annonce
      * 
      * @Route("/ads/new", name="ads_create")
+     * @IsGranted("ROLE_USER")
      *
      * @return Response
      */
@@ -75,6 +78,7 @@ class AdController extends AbstractController
      * Permet d'afficher le formulaire d'édition
      * 
      * @Route("/ads/{slug}/edit", name="ads_edit")
+     * @Security("is_granted('ROLE_USER') and user === ad.getAuthor()", message="Cette annonce ne vous appartient pas, vous ne pouvez pas la modifier !")
      *
      * @return Response
      */
@@ -124,5 +128,30 @@ class AdController extends AbstractController
         return $this->render('ad/show.html.twig', [
             'ad' => $ad
         ]);
+    }
+
+    /**
+     * Permet de supprimer une annonce
+     * 
+     * 
+     * @Route("/ads/{slug}/delete", name="ads_delete")
+     * 
+     * @Security("is_granted('ROLE_USER') and user === ad.getAuthor()", message="Vous n'avez pas le droit d'accéder à cette ressource")
+     *
+     * @param Ad $ad
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function delete(Ad $ad, ObjectManager $manager)
+    {
+        $manager->remove($ad);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "Votre annonce <strong>{$ad->getTitle()}</strong> a bien été supprimée !"
+        );
+
+        return $this->redirectToRoute("ads_index");
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -22,6 +23,24 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create("fr-FR");
 
+        //ON GERE LES ROLES
+
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        $adminUser = new User();
+        $adminUser->setFirstName('Lior')
+                  ->setLastName('Chamla')
+                  ->setEmail('lior@symfony.com')
+                  ->setHash($this->encoder->encodePassword($adminUser, 'password'))
+                  ->setPicture('https://avatars.io/twitter/LiiorC')
+                  ->setIntroduction($faker->sentence())
+                  ->setDescription("<p>" . join("</p><p>", $faker->paragraphs(3)) . "</p>")
+                  ->addUserRole($adminRole);
+
+        $manager->persist($adminUser);
+
         //BOUCLE POUR UTILISATEUR
 
         $users = [];
@@ -36,7 +55,7 @@ class AppFixtures extends Fixture
             $picture = 'https://randomuser.me/api/portraits/';
             $pictureId = $faker->numberBetween(1, 99) . '.jpg';
 
-            $picture = $picture . ($genre == 'male' ? 'male/' : 'women/') . $pictureId;
+            $picture = $picture . ($genre == 'male' ? 'men/' : 'women/') . $pictureId;
 
             $hash = $this->encoder->encodePassword($user, 'password');
 
